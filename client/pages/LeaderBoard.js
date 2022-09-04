1fe35bd79f1e9cbc01c2e67dc1176f717954d3b2
MichaelMezhiritskiy <mezhmichael@gmail.com>
2022-09-04 10:29:39 -0400

    Basic leaderboard finish

---
+++ client/pages/LeaderBoard.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import Competitors from './Competitors';
+
+const LeaderBoard = (props) => {
+    const [ places, setPlaces ] = useState([]);
+
+    useEffect(() => {
+        console.log('useEffect triggered')
+        fetch('/board')
+        .then(response => response.json())
+        .then(response => {
+            console.log(response.sort((a,b) => b.score - a.score))
+            const finalList = [];
+            let key = 1;
+            for (const element of response) {
+                finalList.push(<Competitors num={key} name={element.username} score={element.score}/>)
+                key++;
+            }
+            setPlaces(finalList);
+        })
+        .catch((err) => console.log(err.json()));
+    }, []);
+
+  return (
+    <div>
+        {places}
+    </div>
+  )
+}
+
+export default LeaderBoard
