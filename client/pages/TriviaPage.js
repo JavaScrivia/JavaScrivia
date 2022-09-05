1cc1e62364aa7797800cc79844a64e3a24a099fa
Kara Chisholm <kkchisholm@gmail.com>
2022-09-05 12:48:58 -0400

    Display answer explanation onClick
    Co-authored-by: Michael Mezhiritskiy <MichaelMezhiritskiy@users.noreply.github.com>

--- client/pages/TriviaPage.js
+++ client/pages/TriviaPage.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import {Routes, Route, useNavigate} from 'react-router-dom';
 import LeaderBoard from './LeaderBoard';
+import Answer from './Answer';
 
 const TriviaPage = () => {
-    
+    const [ explanation, setExplanation ] = useState(false);
+
     const [state, setState] = useState({ 
         i: 0,
         codeSnippet: '',
@@ -14,7 +16,10 @@ const TriviaPage = () => {
             B: '',
             C: '',
             D: ''
-        }
+        },
+        correctAnswer: '',
+        answerExplanation: '',
+
     })
     
     
@@ -32,19 +37,29 @@ const TriviaPage = () => {
             //answerExplanation: data.questions[state.i].answerExplanation,
             //}))
             //
+            // .then(console.log(res))
             // PROTOTYPE OF RENDERING BUTTON OPTIONS:
-            .then(data => setState({
+            .then(data => {
+                // state.questions[state.i].codeSnippet.replaceAll(/(?:\r\n|\r|\n)/g, '<br />');
+                setState({
                 i: state.i + 1,
                 codeSnippet: data.questions[state.i].codeSnippet,
             currentQuestion: data.questions[state.i].question,
-             answerOptions: data.questions[state.i].answerOptions
-            }))
-
-
-
+             answerOptions: data.questions[state.i].answerOptions,
+             correctAnswer: data.questions[state.i].correctAnswer,
+             answerExplanation: data.questions[state.i].answerExplanation
+                
+            })
+            setExplanation(false);
+        })
 
             .then(data => console.log(data.questions[state.i]));
-        //console.log('this is i', state.i)
+        console.log(data)
+    }
+
+    const changeBoolean = () => {
+        console.log(state.answerOptions.D)
+        setExplanation(true);
     }
 
     return (
@@ -53,19 +68,20 @@ const TriviaPage = () => {
             <div className="mainContainer">
                 <div className="triviaContainer">
                     <div className="codeSnippet">
-                        <p> Prompt: {state.codeSnippet}</p>
+                        <p> Prompt: <br/>{state.codeSnippet}</p>
                     </div>
                     <div className="questionArea">
                         <p>Question: {state.currentQuestion}</p>
                     </div>
                     <div className="answerOptions">
-                    <button>A <span> {state.answerOptions.A} </span></button>
-                    <button>B <span> {state.answerOptions.B} </span></button>
-                    <button>C <span> {state.answerOptions.C} </span></button>
-                    <button>D <span> {state.answerOptions.D} </span></button>
+                        { state.answerOptions.A && <button onClick={changeBoolean}>A <span> {state.answerOptions.A} </span></button> }
+                        { state.answerOptions.B && <button onClick={changeBoolean}>B <span> {state.answerOptions.B} </span></button> }
+                        { state.answerOptions.C && <button onClick={changeBoolean}>C <span> {state.answerOptions.C} </span></button> }
+                        { state.answerOptions.D && <button onClick={changeBoolean}>D <span> {state.answerOptions.D} </span></button> }
                     </div>
                     <div className="explanation">
-                        <p>Explanation of answer: {state.answerExplanation}</p>
+                        {explanation && <Answer correctAnswer={state.correctAnswer} explanation={state.answerExplanation}/>}
+                        
                     </div> 
                 </div>
                 <div className="leaderboardContainer">
